13d76f0e9121123a8faf6185776ab127172c5f83
Charu Aggarwal <01charuaggarwal@gmail.com>
2023-10-04 21:49:06 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# budget_tracker
+ created a budget tracker that tracks and efficiently manages budgets using react, html,css
---
+++ src/components/TotalBudgetCard.js
@@ -0,0 +1,16 @@
+import { useBudgets } from "../contexts/BudgetsContext"
+import BudgetCard from "./budgetCard"
+
+export default function TotalBudgetCard() {
+  const { expenses, budgets } = useBudgets()
+
+  if (!expenses || !budgets || expenses.length === 0 || budgets.length === 0) {
+    return null; 
+  }
+
+  const amount = expenses.reduce((total, expense) => total + expense.amount, 0)
+  const max = budgets.reduce((total, budget) => total + budget.max, 0)
+  if (max === 0) return null
+
+  return <BudgetCard amount={amount} name="Total" gray max={max} hideButtons />
+}
